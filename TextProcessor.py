0028a6df2c17287a8d8408018a36bcb528cf075e
Harald Hamrin Reinhed <haraldhr@kth.se>
2025-05-08 09:59:23 +0200

    bugfix

--- TextProcessor.py
+++ TextProcessor.py
@@ -9,29 +9,33 @@ class TextProcessor:
     def __init__(self, data_str):
         self.data_str = data_str
         self.data_lower = data_str.lower()
-        self.unique_chars = set(data_str)  # Collect unique characters in the input data
         self.word_dict = self._create_word_dict()
 
     def _create_word_dict(self):
+        """Creates a dictionary with all unique words as keys"""
         words = re.split(r"[,\$\?'\n&;:\s\-\.\!]+", self.data_lower)
+        #print(self.data_lower)
         unique_words = set(words)
         return {word: 1 for word in unique_words}
 
     def correctly_spelt_count(self, text):
+        """Returns the fraction of correctly spelt words."""
         text_lower = text.lower()
-        words = re.findall(r"[,\$\?'\n&;:\s\-\.\!]+", text_lower)
-
+        words = re.split(r"[,\$\?'\n&;:\s\-\.\!]+", text_lower)
         counter = 0
         for word in words:
             if word in self.word_dict:
                 counter += 1
-        return counter
+        if counter == 0: return 0
+
+        return counter / len(words)
+
 
 """
 # Example usage
-data_str = "This is an example sentence. Some words here are correct, others may not be."
+data_str, _ = ReadData()
 processor = TextProcessor(data_str)
 
 # Test the methods
-print("Correctly spelt count:", processor.correctly_spelt_count("aaaaaaaaaaaaaaaaa blaze. linger, ear fur"))
+print("Correctly spelt count:", processor.correctly_spelt_count("aaaaaaaaaaaaaaaaa blaze. linger, ear for"))
 """
