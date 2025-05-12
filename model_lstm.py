5ec058ec1eea3cf741e783d8791b2675b6377b2e
Harald Hamrin Reinhed <haraldhr@kth.se>
2025-05-12 09:58:45 +0200

    fixed output weights

--- model_lstm.py
+++ model_lstm.py
@@ -31,8 +31,10 @@ class LSTM:
         # Initializing trainable parameters
         self.W_all = None # Contains a vector with all W weight matrices
         self.U_all = None # Contains a vector will all U weight matrices.
+        self.V = None # output weight matrix
         self.B = None
         self.C = None
+        self.D = None
 
         # Dynamic parameters.
         self.memory_vec = None
@@ -54,11 +56,14 @@ class LSTM:
         '''
         self.W_all = torch.empty(4, self.m, self.m, dtype=torch.float64, requires_grad=True)
         self.U_all = torch.empty(4, self.m, self.K, dtype=torch.float64, requires_grad=True)
+        self.V = torch.empty(self.K, self.m, dtype=torch.float64, requires_grad=True)
+
 
         # Xavier initialization for all weights.
         for i in range(4):
             torch.nn.init.xavier_uniform_(self.W_all[i])
             torch.nn.init.xavier_uniform_(self.U_all[i])
+        torch.nn.init.xavier_uniform_(self.V)
 
 
 
@@ -84,16 +89,7 @@ class LSTM:
         apply_sigmoid = torch.nn.Sigmoid()
         apply_softmax = torch.nn.Softmax(dim=1)
 
-        # create an empty tensor to store the hidden vector at each timestep
-        Hs = torch.empty(tau, self.m, dtype=torch.float64)
-        As = torch.empty(tau, 4, self.m, dtype=torch.float64)
-        Fs = torch.empty(tau, self.m, dtype=torch.float64) # is the 1 needed here?
-        Os = torch.empty(tau, self.m, dtype=torch.float64)
-        Is = torch.empty(tau, self.m, dtype=torch.float64)
-        Cs = torch.empty(tau, self.m, dtype=torch.float64)
-        C_Hat_s = torch.empty(tau, self.m, dtype=torch.float64)
-
-        As, Fs, Is, Os, Cs, C_Hat_s, Hs = [], [], [], [], [], [], []
+        As, Fs, Is, Os, Cs, C_Hat_s, Hs, St = [], [], [], [], [], [], [], []
 
         E = torch.zeros(4, 4, self.m, self.m, dtype=torch.float64)
         for i in range(4):
@@ -119,6 +115,8 @@ class LSTM:
 
             Hs.append(Os[t] * apply_tanh(Cs[t]))
             hprev = Hs[t].reshape(self.m, 1)
+            #NEW:
+            St.append(torch.matmul(self.V, Hs[t].reshape(self.m, 1)))# + self.D
 
         # Os = torch.matmul(Hs, self.W_o) + self.C
         Hs = torch.stack(Hs, dim=0)  # shape (tau, m, 1)
@@ -127,9 +125,10 @@ class LSTM:
         Is = torch.stack(Is, dim=0)
         Os = torch.stack(Os, dim=0)
         C_Hat_s = torch.stack(C_Hat_s, dim=0)
+        St = torch.stack(St, dim=0)
         Cs = torch.stack(Cs, dim=0)
-        P = apply_softmax(Os)
-
+        P = apply_softmax(St)
+        print(P.shape)
         # compute the loss
         loss = torch.mean(-torch.log(P[np.arange(tau), y]))  # use this line if storing inputs row-wise
         return loss
@@ -137,7 +136,72 @@ class LSTM:
     def backward(self, loss):
         loss.backward()
         return (self.W_all.grad, self.U_all.grad) # unsure if correct.
+    
+    def synth_text(self, x0, n, ind_to_char, char_to_ind, rng, h0 = None):
+        '''
+        Computes the forward pass of the LSTM model to make a prediction.
+        :param x0: the first character
+        :param n: length of the synthesized text
+        :param rng: the previously initialized rng
+        :return:
+        '''
+
+        if h0 is None:
+            h0 = torch.empty(self.m, 1, dtype=torch.float64) # Shape?
+
+        # Initliazing the first hidden layer computaions.
+        ht = h0
+        ct = torch.zeros(1, self.m, dtype=torch.float64)
+
+        ## give informative names to these torch classes
+        apply_tanh = torch.nn.Tanh()
+        apply_sigmoid = torch.nn.Sigmoid()
+        apply_softmax = torch.nn.Softmax()
+
+        hprev = ht
+        cprev = ct
+        x = torch.zeros(1, self.K, dtype=torch.float64)
+        x[0, char_to_ind[x0]] = 1
+        print(x.shape)
+        synth_text = ""
+        for t in range(n):
+            # at will have shape (4xmx1)
+            at = torch.matmul(self.W_all, hprev) + torch.matmul(self.U_all, x.reshape(x.shape[1], 1))
+            #As.append(at.squeeze())
+            # Exa_t will have shape (4xmx1)
+            #NOTE: Might be wrong shape.    
+
+            f_t = apply_sigmoid(at[0]).squeeze() # forget gate.
+            i_t = apply_sigmoid(at[1]).squeeze() # input gate.
+            o_t = apply_sigmoid(at[2]).squeeze() # output gate.
+            c_hat_t = apply_tanh(at[3]).squeeze() # new memory cell.
+            p_t = apply_softmax(o_t)
+            
+            cprev = f_t * cprev + i_t * c_hat_t
+            hprev = (o_t * apply_tanh(cprev)).reshape(self.m, 1)
+
+            # sample (randomly from prob. dist. p_t) and add to o:
+            cp = np.cumsum(p_t.detach().numpy(), axis=0)
+            a = rng.uniform(size=1)
+            ii = np.argmax(cp - a > 0) 
+
+
+            x = torch.zeros(1, self.K)
+            x[0, ii] = 1
+            synth_text += ind_to_char[ii]
+            print("ONE LOOP DONE")
+
+        return synth_text
+
+        
          
+rng = np.random.default_rng()
+# get the BitGenerator used by default
+BitGen = type(rng.bit_generator)
+# use the state from a fresh bit generator
+seed = 1
+rng.bit_generator.state = BitGen(seed).state
+
 
 
 data, unique_chars = ReadData()
@@ -151,6 +215,7 @@ y_seq_indices = [char_to_ind[char] for char in y_seq]
 lstm = LSTM(X)
 
 loss = lstm.forward(X_seq, y_seq_indices)
-grads_W, grads_U = lstm.backward(loss)
-print(grads_W[0])
+#grads_W, grads_U = lstm.backward(loss)
+#print(grads_W[0])
+#lstm.synth_text("a", 25, ind_to_char, char_to_ind, rng)
 
